1c54fd47b8b938cd15e18fcac48ecd57fdebca66
Евгений <eugeny1988@gmail.com>
2022-05-19 16:58:51 +0300

    Change var to val in 1st line and reformat code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,19 @@
-var seconds: Int = 0
+val seconds: Int = 33333
 val oneMinute = 60
 val oneHour = 3600
 val oneDay = oneHour * 24
 val back = "назад"
 fun main() {
-print(agoToText(seconds))
+    print(agoToText(seconds))
 }
+
 fun agoToText(time: Int): String {
     return when (time) {
         in 0..oneMinute - 1 -> "только что"
         in oneMinute..oneHour - 1 -> minutesEnding(time)
         in oneHour..oneDay - 1 -> hoursEnding(time)
-        in oneDay..oneDay*2 - 1 -> "сегодня"
-        in oneDay*2..oneDay*3 - 1 -> "вчера"
+        in oneDay..oneDay * 2 - 1 -> "сегодня"
+        in oneDay * 2..oneDay * 3 - 1 -> "вчера"
         else -> "давно"
     }
 }
@@ -27,15 +28,16 @@ fun minutesEnding(number: Int): String {
             else -> "$minutes минуты $back"
         }
 }
-fun hoursEnding(number: Int): String{
-    val hours = number/3600
+
+fun hoursEnding(number: Int): String {
+    val hours = number / 3600
     if (hours in 5..20) return "$hours часов $back"
     else
-    return when (hours.mod(10)){
-        0, in 5..9 -> "$hours часов $back"
-        1 -> "$hours час $back"
-        else -> "$hours часа $back"
-    }
+        return when (hours.mod(10)) {
+            0, in 5..9 -> "$hours часов $back"
+            1 -> "$hours час $back"
+            else -> "$hours часа $back"
+        }
 }
 //fun daysEnding(number: Int):String{
 //
